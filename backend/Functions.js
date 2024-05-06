7a6e704cda201e1f1d2e3e0761e498eb3fbc8824
Barracudapi <132427435+Barracudapi@users.noreply.github.com>
2024-05-06 21:06:31 +0800

    Install PG for database connectivity

---
+++ backend/Functions.js
@@ -0,0 +1,27 @@
+const { Client } = require('pg');
+
+const client = new Client({
+    user: 'harroldtok',
+    host: 'localhost',
+    database: 'postgres',
+    password: '',
+    port: 5432,
+    });
+
+client.connect()
+.then(() => console.log('Connected to PostgreSQL server'))
+.catch(err => console.error('Error connecting to PostgreSQL server', err));
+
+if (client.connected) {
+    console.log('You are currently connected to PostgreSQL server');
+    } else {
+    console.log('You are not connected to PostgreSQL server');
+    }
+    
+client.query('SELECT * FROM cards')
+.then(res => console.log(res.rows))
+.catch(err => console.error('Error executing query', err));
+
+client.end()
+.then(() => console.log('Connection to PostgreSQL server closed'))
+.catch(err => console.error('Error closing connection', err));
