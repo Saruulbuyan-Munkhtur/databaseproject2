d2c665d7a71ddd896804f3ed4bbd99085546f4ba
Saruul <saruul@Saruuls-MacBook-Pro.local>
2024-05-07 15:49:16 +0800

    added .env file

--- backend/Functions.js
+++ backend/Functions.js
@@ -1,9 +1,11 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('postgres', 'harroldtok', '6969', {
-  host: 'localhost',
-  dialect: 'postgres',
-});
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'postgres',
+  });
+  
 
 module.exports = sequelize;
 
--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,7 +1,9 @@
 const Sequelize = require('sequelize');
+require('dotenv').config();
 
-const sequelize = new Sequelize('postgres', 'harroldtok', '6969', {
-  host: 'localhost',
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   dialect: 'postgres',
 });
 
